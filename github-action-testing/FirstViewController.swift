c18cfc35dd186065333ea9279c8da06798f470db
Gabriel-Azevedo <gabriel.b.azevedo@icloud.com>
2020-09-07 12:42:26 -0300

    Initial Commit

---
+++ github-action-testing/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  github-action-testing
+//
+//  Created by Gabriel Azevedo on 07/09/20.
+//  Copyright © 2020 Gabriel Azevedo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ github-action-testing/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  github-action-testing
+//
+//  Created by Gabriel Azevedo on 07/09/20.
+//  Copyright © 2020 Gabriel Azevedo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

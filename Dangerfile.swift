521aa432b5e196850a19a9a67b278d5ab3c101e8
Gabriel-Azevedo <gabriel.b.azevedo@icloud.com>
2020-09-07 17:29:46 -0300

    action

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,27 +1,49 @@
-import Danger
+# PR is a work in progress and shouldn't be merged yet
+warn "PR is classed as Work in Progress" if github.pr_title.include? "[WIP]"
 
-let danger = Danger()
+# Warn when there is a big PR
+warn "Big PR, consider splitting into smaller" if git.lines_of_code > 500
 
-// MARK: - Warn when there is a big PR (both additions and deletion)
+# Ensure a clean commits history
+if git.commits.any? { |c| c.message =~ /^Merge branch '#{github.branch_for_base}'/ }
+  fail "Please rebase to get rid of the merge commits in this PR"
+end
 
-let minimalPRlimit = 800
-let mediumPRlimit = 2000
-let extremePRlimit = 5000
-let additions = danger.github.pullRequest.additions ?? 0
-let deletions = danger.github.pullRequest.deletions ?? 0
-let changes = additions + deletions
+# Mainly to encourage writing up some reasoning about the PR, rather than
+# just leaving a title
+if github.pr_body.length < 5
+  fail "Please provide a summary in the Pull Request description"
+end
 
-switch changes {
-case let c where c >= extremePRlimit:
-    warn("You gotta be kinding me with this HUGE PR! Automatic review starting in 3, 2, 1...")
-case let c where c >= mediumPRlimit:
-    warn("Avoid big pull requests. Pixote's DODÔ HATES big PRs. Next time, try to break it out in multiple ones")
-case let c where c >= minimalPRlimit:
-    warn("Try to avoid big pull requests. In order to it make easier to read and review, you can break in multiple PRs")
-default:
-    break
-}
+# If these are all empty something has gone wrong, better to raise it in a comment
+if git.modified_files.empty? && git.added_files.empty? && git.deleted_files.empty?
+  fail "This PR has no changes at all, this is likely an issue during development."
+end
 
-// MARK: - Linter changed files (${PODS_ROOT}/SwiftLint/swiftlint)
+has_app_changes = !git.modified_files.grep(/ProjectName/).empty?
+has_test_changes = !git.modified_files.grep(/ProjectNameTests/).empty?
 
-SwiftLint.lint(inline: true, swiftlintPath: "Pods/SwiftLint/swiftlint")
+# If changes are more than 10 lines of code, tests need to be updated too
+if has_app_changes && !has_test_changes && git.lines_of_code > 10
+  fail("Tests were not updated", sticky: false)
+end
+
+# Info.plist file shouldn't change often. Leave warning if it changes.
+is_plist_change = git.modified_files.sort == ["ProjectName/Info.plist"].sort
+
+if !is_plist_change
+  warn "Plist changed, don't forget to localize your plist values"
+end
+
+podfile_updated = !git.modified_files.grep(/Podfile/).empty?
+
+# Leave warning, if Podfile changes
+if podfile_updated
+  warn "The `Podfile` was updated"
+end
+
+# This is swiftlint plugin. More info: https://github.com/ashfurrow/danger-swiftlint
+#
+# This lints all Swift files and leave comments in PR if
+# there is any issue with linting
+swiftlint.lint_files inline_mode: true
